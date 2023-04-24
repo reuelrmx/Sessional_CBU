86f9ff347f382303f3172459fe271f50fa683c61
reuelrmx <131676290+reuelrmx@users.noreply.github.com>
2023-04-24 15:08:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateCountdown() {
+  const countDownDate = new Date("August 28, 2023 00:00:00").getTime();
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector(".countdown-number.days .countdown-value").textContent = days;
+  document.querySelector(".countdown-number.hours .countdown-value").textContent = hours;
+  document.querySelector(".countdown-number.minutes .countdown-value").textContent = minutes;
+  document.querySelector(".countdown-number.seconds .countdown-value").textContent = seconds;
+}
+
+setInterval(updateCountdown, 1000);
